12e4a9f7edf67db662447692f34b5f7fb7e4b94b
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-05-08 19:31:58 +0600

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// verify JWT (json web token)
+// verify JWT
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
